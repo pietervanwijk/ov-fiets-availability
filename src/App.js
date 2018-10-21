4e342693ee93b912135b5f81d6b5e27c4f9bfc47
pieter <hello@pietervanwijk.com>
2018-10-21 22:32:39 +0200

    autosuggest

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import Autosuggest from 'react-autosuggest';
 
 class App extends Component {
-  render() {
+  constructor () {
+    super() 
+    this.state = {
+      goals: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (playerName) {
+     console.log('function triggered') 
+     axios.get('http://fiets.openov.nl/locaties.json')
+    .then(response => this.setState({goals: response.data.result[0].player_goals}))
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <h1>Goals {this.state.goals}</h1>
+        </div>
+        <div className='button__container'>
+          <button className='button' onClick={() => this.handleClick('van persie robin')}>Robin van Persie</button>
+        </div>
+        <div className='button__container'>
+          <button className='button' onClick={() => this.handleClick('toornstra jens')}>Jens Toornstra</button>
+        </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
