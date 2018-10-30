4ca93abc159541a9a122a7d2df07799c8eef1483
pieter <hello@pietervanwijk.com>
2018-10-30 14:50:43 +0100

    server back to port 5000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(5001, function(){
-    console.log('Server running on 5001...');
+connect().use(serveStatic(__dirname + '/public')).listen(5000, function(){
+    console.log('Server running on 5000...');
 });
--- src/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(5001, function(){
-    console.log('Server running on 5001...');
-});
